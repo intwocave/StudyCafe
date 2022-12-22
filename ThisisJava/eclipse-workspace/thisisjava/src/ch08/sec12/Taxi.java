d06f9e12f052dc71c4d09f9c29b2817c9d80870c
intwocave <intwocave@gmail.com>
2022-12-22 16:01:07 +0900

    2022.12.22. 16:01

---
+++ ThisisJava/eclipse-workspace/thisisjava/src/ch08/sec12/Taxi.java
@@ -0,0 +1,11 @@
+package ch08.sec12;
+
+public class Taxi implements Vehicle {
+	public void run() {
+		System.out.println("Bus Go Go!!");
+	}
+	
+	public void goFaster() {
+		System.out.print("Go Faster pls");
+	}
+}
